a444b4b5c06ec839f7d5964ab1a841cc3f9a359f
Sam B <samuel.barry.b@hotmail.com>
2021-01-07 15:22:04 +0800

    Removed snippet dates, added snippet descriptions

--- assets/js/gist.js
+++ assets/js/gist.js
@@ -1,33 +1,33 @@
 
-var id = $("#gistScript").data("gist-id"),
-    gist,
-    date;
+// var id = $("#gistScript").data("gist-id"),
+//     gist,
+//     date;
 
-function setDate(d, className) {
-    $(".snippet__date span").html(d).parent().addClass(className);
+// function setDate(d, className) {
+//     $(".snippet__date span").html(d).parent().addClass(className);
 
-}
+// }
 
-async function processGist(g) {
-    gist = await g.data;
-    date = moment(gist.updated_at).format("DD MMM YYYY");
-    sessionStorage.setItem("gist." + id, date);
+// async function processGist(g) {
+//     gist = await g.data;
+//     date = moment(gist.updated_at).format("DD MMM YYYY");
+//     sessionStorage.setItem("gist." + id, date);
 
-    setDate(date, "set");
-}
+//     setDate(date, "set");
+// }
 
 
-if (id) {
-    if (!sessionStorage.getItem("gist." + id))
-        $.ajax({
-            url: 'https://api.github.com/gists/' + id,
-            dataType: 'jsonp',
-            headers: {
-                "Authorisation": "token 3963b3bb9b0f16e07b19792aab4327a93fffa576"
-            },
-            success: processGist
-        });
-    else
-        setDate(sessionStorage.getItem("gist." + id), "saved");
+// if (id) {
+//     if (!sessionStorage.getItem("gist." + id))
+//         $.ajax({
+//             url: 'https://api.github.com/gists/' + id,
+//             dataType: 'jsonp',
+//             headers: {
+//                 "Authorisation": "token 3963b3bb9b0f16e07b19792aab4327a93fffa576"
+//             },
+//             success: processGist
+//         });
+//     else
+//         setDate(sessionStorage.getItem("gist." + id), "saved");
 
-}
+// }
